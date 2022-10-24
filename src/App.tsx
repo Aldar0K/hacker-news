c91109676fed0fc8c61594a424fa59544958e8c3
Aldar <aldarkonov@mail.ru>
2022-10-24 18:24:58 +0300

    feat: apply eslint and prettier, setup tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Button } from 'antd';
 function App() {
   return (
     <div>
-      <Button type='primary'>Button</Button>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
