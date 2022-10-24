cc75ad876b0ad75a2f093833d58391cbae6945f9
Aldar <aldarkonov@mail.ru>
2022-10-24 16:54:42 +0300

    feat: add and setup ant design ui framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div>
-      Hacker News
+      <Button type='primary'>Button</Button>
     </div>
   );
 }
