7b82451bb734a24f8a633255442567852c1892a6
Aldar <aldarkonov@mail.ru>
2022-10-25 12:36:33 +0300

    fix: return the necessary files to support css modules

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
+import styles from './Header.module.css';
 import { Layout, Typography } from 'antd';
 
 const { Header } = Layout;
 
 const _Header: FC = () => {
   return (
-    <Header style={{ textAlign: 'center' }}>
-      <Typography.Title level={1} style={{ color: 'white' }}>
+    <Header className={styles.container}>
+      <Typography.Title level={1} style={{ color: 'white', marginBottom: '0' }}>
         Hacker News
       </Typography.Title>
     </Header>
