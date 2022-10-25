c82fe3105e59f70d7a5218c00052e3bc0bbec591
Aldar <aldarkonov@mail.ru>
2022-10-25 22:45:40 +0300

    feat: add urls to constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://hacker-news.firebaseio.com/v0';
+export const ITEM_URL = `${BASE_URL}/item`;
+export const NEWSTORIES_URL = `${BASE_URL}/newstories`;
+export const TOPSTORIES_URL = `${BASE_URL}/topstories `;
+export const BESTSTORIES_URL = `${BASE_URL}/beststories `;
