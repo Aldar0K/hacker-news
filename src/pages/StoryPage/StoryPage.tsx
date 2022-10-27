7f2463a84ce4fd968b8d0133929694c5fef34fad
Aldar <aldarkonov@mail.ru>
2022-10-27 20:25:04 +0300

    feat: update utils

--- src/pages/StoryPage/StoryPage.tsx
+++ src/pages/StoryPage/StoryPage.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './StoryPage.module.css';
 import { useParams } from 'react-router-dom';
-import { Typography } from 'antd';
+import { Skeleton, Typography } from 'antd';
+import { IStory } from 'models';
+import { fetchStoryById } from 'API';
+import { getDateFromUnixTimestamp } from 'utils';
 
 interface IParams {
   id: string;
 }
 
 const StoryPage = () => {
-  const { id } = useParams<IParams>();
+  const [story, setStory] = useState<IStory | null>(null);
+  const { id: storyId } = useParams<IParams>();
+
+  useEffect(() => {
+    fetchStoryById(Number(storyId))
+      .then((data) => {
+        setStory(data);
+      })
+      .catch((err: Error) => {
+        console.log(err.message);
+        // setError(err);
+      });
+  }, [storyId]);
 
   return (
     <main className={`main ${styles.main}`}>
       <div className={`container ${styles.container}`}>
-        <Typography.Title level={2}>ID: {id}</Typography.Title>
+        {story && (
+          <>
+            <Typography.Title level={2}>{story.title}</Typography.Title>
+            <Typography.Title level={3}>
+              <a href={story.url} target="_blank" rel="noreferrer">
+                Link
+              </a>
+            </Typography.Title>
+            <Typography.Title level={3}>
+              Posted: {getDateFromUnixTimestamp(story.time)}
+            </Typography.Title>
+            <Typography.Title level={3}>Author: {story.by}</Typography.Title>
+            <Typography.Title level={3}>Comments: {story.descendants}</Typography.Title>
+          </>
+        )}
+        {/* {error && <Typography.Title level={4}>Score: {error.message}</Typography.Title>} */}
+        {/* {!story && !error && <Skeleton />} */}
+        {!story && <Skeleton />}
       </div>
     </main>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,9 @@
-import { IStory } from 'models';
-
-export const extractFields = (story: IStory) => ({
-  id: story.id,
-  title: story.title,
-  rating: story.score,
-  author: story.by,
-  url: story.url,
-  date: story.time,
-});
-
-export const getDateFromUnixTimestamp = (value: number) => {
+export const getDateFromUnixTimestamp = (value: number): string => {
   const date = new Date(value * 1000);
-  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${day}.${month < 10 ? `0${month}` : month}.${year}`;
 };
